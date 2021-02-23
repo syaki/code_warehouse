226afcd4dac8a1742f2e0367ad8d615f6f2eca2c
syaki <syaki07@outlook.com>
2021-02-23 20:02:06 +0800

    feat(algs4): add method InsertionX in insertion_sort

--- algs4/insertion_sort.go
+++ algs4/insertion_sort.go
@@ -12,3 +12,31 @@ func Insertion(arr []int) {
 		}
 	}
 }
+
+// InsertionX rearranges the array that using half exchanges instead of full exchanges to reduce data movement.
+func InsertionX(arr []int) {
+	arrLen := len(arr)
+
+	// put smallest element in position to serve as sentinel
+	exchanges := 0
+	for i := arrLen - 1; i > 0; i-- {
+		if arr[i] < arr[i-1] {
+			arr[i], arr[i-1] = arr[i-1], arr[i]
+			exchanges++
+		}
+	}
+
+	if exchanges == 0 {
+		return
+	}
+	// insertion sort with half-exchanges
+	for i := 2; i < arrLen; i++ {
+		temp := arr[i]
+		j := i
+		for temp < arr[j-1] {
+			arr[j] = arr[j-1]
+			j--
+		}
+		arr[j] = temp
+	}
+}
--- algs4/insertion_sort_test.go
+++ algs4/insertion_sort_test.go
@@ -3,9 +3,18 @@ package algs4
 import "testing"
 
 func TestInsertion(t *testing.T) {
-	arr := []int{3, 4, 7, 1, 0, 5, 2, 6}
-	Insertion(arr)
-	if !IsSorted(arr) {
-		t.Errorf("failed")
-	}
+	t.Run("Test Insertion", func(t *testing.T) {
+		arr := []int{3, 4, 7, 1, 0, 5, 2, 6}
+		Insertion(arr)
+		if !IsSorted(arr) {
+			t.Errorf("failed")
+		}
+	})
+	t.Run("Test InsertionX", func(t *testing.T) {
+		arr := []int{3, 4, 7, 1, 0, 5, 2, 6}
+		InsertionX(arr)
+		if !IsSorted(arr) {
+			t.Errorf("failed")
+		}
+	})
 }
